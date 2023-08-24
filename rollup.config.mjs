d69c9285b4956714893d58e2073e1376176c1109
Gvozd <gvozdev.viktor@gmail.com>
2023-08-24 22:51:12 +0300

    build(rollup): bundling with rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,22 @@
+import ts from 'rollup-plugin-ts';
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: 'dist/index.mjs',
+        format: 'es',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      ts(),
+    ]
+  }
+];
