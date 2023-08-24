33d78e33ac228bd5a961c7af3b5bddc494f66cf0
Gvozd <gvozdev.viktor@gmail.com>
2023-08-24 22:54:40 +0300

    build(tsc): use isolatedModules

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // Core Interfaces
-export {default as IExecutable} from "./core/IExecutable";
+export type {default as IExecutable} from "./core/IExecutable";
 
 // Chain of responsibility
-export {default as IChainOfResponsibilityStep} from "./chainOfResponsibility/IChainOfResponsibilityStep";
+export type {default as IChainOfResponsibilityStep} from "./chainOfResponsibility/IChainOfResponsibilityStep";
 export {default as ChainOfResponsibilityStep} from "./chainOfResponsibility/ChainOfResponsibilityStep";
 export {default as ChainOfResponsibility} from "./chainOfResponsibility/ChainOfResponsibility";
