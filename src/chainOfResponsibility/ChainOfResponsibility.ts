11a8eb8cf636a08a9ecccb5b0059237585dab154
Eugene <eugenezvg@gmail.com>
2023-09-03 16:35:36 +0200

    chore(cor): Use interface instead of chain class for constructing a chain

--- src/chainOfResponsibility/ChainOfResponsibility.ts
+++ src/chainOfResponsibility/ChainOfResponsibility.ts
@@ -1,12 +1,13 @@
 import ChainOfResponsibilityStep from './ChainOfResponsibilityStep';
+import IChainOfResponsibilityStep from './IChainOfResponsibilityStep';
 
 export default class ChainOfResponsibility extends ChainOfResponsibilityStep {
-    constructor(steps: ChainOfResponsibilityStep[]) {
+    constructor(steps: IChainOfResponsibilityStep[]) {
         super();
         this.createChain(steps);
     }
 
-    private createChain(steps: ChainOfResponsibilityStep[]) {
+    private createChain(steps: IChainOfResponsibilityStep[]) {
         for (let i = 0; i < steps.length; i++) {
             const step = i === 0 ? this : steps[i - 1];
             step.setNext(steps[i]);
