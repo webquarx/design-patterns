33f4c2b7383db1e70388191f16c360b08c9c42f4
Eugene <eugenezvg@gmail.com>
2023-09-08 09:31:58 +0200

    chore(jest): eslint warnings

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+    preset: 'ts-jest',
+    testEnvironment: 'node',
 
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageProvider: 'v8',
 };
