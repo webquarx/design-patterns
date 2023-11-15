a8ca1cc0d0a6131b451f2b0f10c69961de833271
Eugene <eugenezvg@gmail.com>
2023-11-15 22:18:23 +0100

    feat(cor): Move the execution implementation to the protected executeNext method to be called when overriding in a long inheritance chain

--- src/chainOfResponsibility/ChainOfResponsibilityStep.ts
+++ src/chainOfResponsibility/ChainOfResponsibilityStep.ts
@@ -18,11 +18,20 @@ export default abstract class ChainOfResponsibilityStep implements IChainOfRespo
     }
 
     async execute(...args: any[]): Promise<any> {
+        return await this.executeNext(...args);
+    }
+
+    protected async executeNext(...args: any[]): Promise<any> {
         if (this.nextStep) {
             return await this.nextStep.execute(...args);
         }
+        return this.getResult(...args);
+    }
+
+    // eslint-disable-next-line class-methods-use-this
+    private getResult(...args: any[]) {
         if (args.length === 0) {
-            return await Promise.resolve();
+            return undefined;
         }
         return args.length === 1 ? args[0] : args;
     }
