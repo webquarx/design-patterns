ea1e94f7b378a026ea1ea0e63cda3e33bc4c2314
Eugene <eugenezvg@gmail.com>
2023-11-15 22:14:51 +0100

    fix(cor): correct exporting IConditionalChainOfResponsibility interface

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export type { default as IExecutable } from './core/IExecutable';
 // Chain of responsibility
 export type { default as IChainOfResponsibilityStep } from './chainOfResponsibility/IChainOfResponsibilityStep';
 export { default as ChainOfResponsibilityStep } from './chainOfResponsibility/ChainOfResponsibilityStep';
-export type { default as IConditionalChainOfResponsibility } from './chainOfResponsibility/IChainOfResponsibilityStep';
+export type { IConditionalChainOfResponsibility } from './chainOfResponsibility/IChainOfResponsibilityStep';
 export { default as ChainOfResponsibilityConditionalStep } from './chainOfResponsibility/ChainOfResponsibilityConditionalStep';
 export { default as ChainOfResponsibility } from './chainOfResponsibility/ChainOfResponsibility';
 export { default as useChain } from './chainOfResponsibility/useChain';
