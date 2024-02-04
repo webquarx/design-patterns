2e7485a83214cbcc2af395982ae32dc542aa5798
Eugene <eugenezvg@gmail.com>
2024-02-04 18:51:42 +0100

    test(invoker): test all tasks to ensure they have the key

--- src/invoker/test/useTask.test.ts
+++ src/invoker/test/useTask.test.ts
@@ -1,19 +1,37 @@
 import useTask from '../useTask';
 import Invoker from '../Invoker';
 import { useCommand } from '../../command/useCommand';
+import ICommand from '../../command/ICommand';
 
 describe('useTask', () => {
-    it('should return a task with the default status', async () => {
-        const cmd = useCommand(
+    let cmd: ICommand;
+
+    beforeEach(() => {
+        cmd = useCommand(
             async () => await new Promise((resolve) => {
                 setTimeout(resolve, 50);
             }),
         );
+    });
+
+    it('should return a task with the default status', async () => {
         const task = useTask(cmd);
         expect(task.status).toEqual('idle');
 
         const invoker = new Invoker(task);
         await invoker.parallel();
         expect(task.status).toEqual('fulfilled');
+    });
+
+    it('should return a task with non-empty keys', async () => {
+        const task = useTask(cmd);
+        expect(task.key).toContain('-');
+
+        const invoker = new Invoker([task, cmd]);
+        await invoker.parallel();
+
+        const { tasks } = invoker;
+        expect(tasks[0].key).toEqual(task.key);
+        expect(tasks[1].key).toContain('-');
     });
 });
